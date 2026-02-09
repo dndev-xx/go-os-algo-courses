4c6051cdac88738feb1c611501ee2d4c58ab874c
Никишин Данил Дмитриевич <ddnikishin@gid.ru>
2026-02-09 10:45:06 +0300

    feat(hw02): impl happy ticket case
    * feat(hw01): init project
    
    * feat(hw02): change ci.yml
    
    * feat(hw02): change ci.yml
    
    * feat(hw02): change ci.yml
    
    * feat(hw02): change ci.yml
    
    * feat(hw02): change ci.yml
    
    * feat(hw02): change ci.yml
    
    * feat(hw02): change ci.yml
    
    * feat(hw02): change ci.yml
    
    * feat(hw02): change ci.yml
    
    * feat(hw02): change ci.yml
    
    * feat(hw02): change ci.yml
    
    * feat(hw02): change ci.yml
    
    * feat(hw02): change ci.yml
    
    * feat(hw02): change ci.yml
    
    * feat(hw02): change ci.yml
    
    * feat(hw02): change ci.yml
    
    * feat(hw02): change ci.yml
    
    * feat(hw01): impl first test
    
    * feat(hw01): impl first test
    
    ---------
    
    Co-authored-by: Никишин Данил Дмитриевич <ddnikishin@gid.ru>

--- internal/basic-module/happy_ticket.go
+++ internal/basic-module/happy_ticket.go
@@ -3,11 +3,12 @@ package basicmodule
 import (
 	"errors"
 	"strconv"
+	"unicode/utf8"
 )
 
 const (
 	_ticketCnt = 6
-	div        = 10
+	_div       = 10
 )
 
 var errTicketLimitDigits = errors.New("ticket must have 6 digits")
@@ -22,15 +23,59 @@ func IsHappyTicket(ticket int) (bool, error) {
 	lsum := 0
 	rsum := 0
 	for i := range _ticketCnt {
-		cur := ticket % div
+		cur := ticket % _div
 		if i < _ticketCnt>>1 {
 			lsum += cur
 		} else {
 			rsum += cur
 		}
-		ticket /= div
+		ticket /= _div
 	}
 	return lsum == rsum, nil
+}
+
+// NHappyTickets returns the number of happy tickets in the range [a, b].
+// return: count the number of happy tickets
+func NHappyTickets(digits int) int64 {
+	arr := sumArr(digits)
+	if len(arr) == 0 {
+		return 0
+	}
+	var rsl int64
+
+	for _, v := range arr {
+		rsl += v * v
+	}
+	return rsl
+}
+
+func sumArr(n int) []int64 {
+	if n <= 0 {
+		return nil
+	}
+
+	s := n*9 + 1
+	arr := make([]int64, s)
+	if n == 1 {
+		for i := range arr {
+			arr[i] = 1
+		}
+		return arr
+	}
+	prev := sumArr(n - 1)
+	for i := range 10 {
+		for j := range prev {
+			if i+j < len(arr) {
+				arr[i+j] += prev[j]
+			}
+		}
+	}
+	return arr
+}
+
+// Len func that returns the length of the string.
+func Len(sym string) int {
+	return utf8.RuneCountInString(sym)
 }
 
 func hasSixDigits(num int) bool {
--- internal/basic-module/happy_ticket_test.go
+++ internal/basic-module/happy_ticket_test.go
@@ -52,3 +52,119 @@ func TestIsHappyTicket(t *testing.T) {
 		})
 	}
 }
+
+func TestLen(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		sym     string
+		want    int
+		wantErr bool
+	}{
+		{
+			name:    "test_0",
+			sym:     "1234567890123456789012345678901234567890123456789012345678901234",
+			want:    64,
+			wantErr: false,
+		},
+		{
+			name:    "test_1",
+			sym:     "12345678",
+			want:    8,
+			wantErr: false,
+		},
+		{
+			name:    "test_2",
+			sym:     "",
+			want:    0,
+			wantErr: false,
+		},
+		{
+			name:    "test_3",
+			sym:     "$",
+			want:    1,
+			wantErr: false,
+		},
+		{
+			name:    "test_4",
+			sym:     "abcdefghijklmnoprstuvwxyz",
+			want:    25,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := basicmodule.Len(tt.sym)
+			if tt.wantErr {
+				t.Fatal("Len() succeeded unexpectedly")
+			}
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestNHappyTickets(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		digits int
+		want   int64
+	}{
+		{
+			name:   "test_0",
+			digits: 1,
+			want:   10,
+		},
+		{
+			name:   "test_1",
+			digits: 2,
+			want:   670,
+		},
+		{
+			name:   "test_2",
+			digits: 3,
+			want:   55252,
+		},
+		{
+			name:   "test_3",
+			digits: 4,
+			want:   4816030,
+		},
+		{
+			name:   "test_4",
+			digits: 5,
+			want:   432457640,
+		},
+		{
+			name:   "test_5",
+			digits: 6,
+			want:   39581170420,
+		},
+		{
+			name:   "test_6",
+			digits: 7,
+			want:   3671331273480,
+		},
+		{
+			name:   "test_7",
+			digits: 8,
+			want:   343900019857310,
+		},
+		{
+			name:   "test_8",
+			digits: 9,
+			want:   32458256583753952,
+		},
+		{
+			name:   "test_9",
+			digits: 10,
+			want:   3081918923741896840,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := basicmodule.NHappyTickets(tt.digits)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
